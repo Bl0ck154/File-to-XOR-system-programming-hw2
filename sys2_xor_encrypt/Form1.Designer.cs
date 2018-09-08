b5583d15d27194dadc9e9553bc9b122a05e18f4c
Bl0ck <zail@dublin.com>
2018-09-08 12:25:28 +0300

    beta

--- sys2_xor_encrypt/Form1.Designer.cs
+++ sys2_xor_encrypt/Form1.Designer.cs
@@ -29,12 +29,117 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.buttonFileChoose = new System.Windows.Forms.Button();
+			this.textBoxKey = new System.Windows.Forms.TextBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonFileChoose
+			// 
+			this.buttonFileChoose.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonFileChoose.Location = new System.Drawing.Point(0, 28);
+			this.buttonFileChoose.Name = "buttonFileChoose";
+			this.buttonFileChoose.Size = new System.Drawing.Size(406, 260);
+			this.buttonFileChoose.TabIndex = 1;
+			this.buttonFileChoose.Text = "Choose file";
+			this.buttonFileChoose.UseVisualStyleBackColor = true;
+			// 
+			// textBoxKey
+			// 
+			this.textBoxKey.Location = new System.Drawing.Point(31, 3);
+			this.textBoxKey.Name = "textBoxKey";
+			this.textBoxKey.Size = new System.Drawing.Size(166, 22);
+			this.textBoxKey.TabIndex = 2;
+			this.textBoxKey.Text = "123456";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.Location = new System.Drawing.Point(0, 294);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(406, 33);
+			this.progressBar1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(0, 5);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(36, 17);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Key:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(204, 5);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(84, 17);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Block bytes:";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Increment = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Location = new System.Drawing.Point(286, 3);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown1.TabIndex = 7;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            4096,
+            0,
+            0,
+            0});
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(406, 327);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.textBoxKey);
+			this.Controls.Add(this.buttonFileChoose);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "XOR Encrypter";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+		private System.Windows.Forms.Button buttonFileChoose;
+		private System.Windows.Forms.TextBox textBoxKey;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.ToolTip toolTip1;
 	}
 }
 
--- sys2_xor_encrypt/Form1.cs
+++ sys2_xor_encrypt/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,157 @@ namespace sys2_xor_encrypt
 {
 	public partial class Form1 : Form
 	{
+		enum ButtonState
+		{
+			START,
+			FILE_CHOOSED,
+			ENCRYPTING,
+			CANCELING
+		};
+
+		ButtonState btnState;
+		ButtonState buttonState
+		{
+			get { return btnState; }
+			set
+			{
+				btnState = value;
+				switch (buttonState)
+				{
+					case ButtonState.START:
+						buttonFileChoose.Invoke(new Action(()=>buttonFileChoose.Text = "Choose file"));
+						break;
+					case ButtonState.FILE_CHOOSED:
+						buttonFileChoose.Text = $"Encrypt!";
+						break;
+					case ButtonState.ENCRYPTING:
+						buttonFileChoose.Text = "PLEASE STOP THIS!!!";
+						break;
+					case ButtonState.CANCELING:
+						buttonFileChoose.Text = "Canceling...";
+						break;
+				}
+			}
+		}
+
+		FileInfo file;
+
 		public Form1()
 		{
 			InitializeComponent();
+			CenterToScreen();
+			buttonFileChoose.Click += ButtonFileChoose_Click;
+		}
+
+		private void ButtonFileChoose_Click(object sender, EventArgs e)
+		{
+			switch(buttonState)
+			{
+				case ButtonState.START:
+					firstStateClickChooseFile();
+					break;
+				case ButtonState.FILE_CHOOSED:
+					secondStateClickToEncrypt();
+					break;
+				case ButtonState.ENCRYPTING:
+					thirdStateClickCancel();
+					break;
+			}
+		}
+		void firstStateClickChooseFile()
+		{
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			if (fileDialog.ShowDialog() == DialogResult.OK)
+			{
+				file = new FileInfo(fileDialog.FileName);
+				buttonState = ButtonState.FILE_CHOOSED;
+				toolTip1.SetToolTip(buttonFileChoose, $"{fileDialog.FileName}");
+			}
+		}
+		void secondStateClickToEncrypt()
+		{
+			if (MessageBox.Show($"Are you sure that you want to encrypt {file.Name} ?", "Confirmation", 
+				MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+			{
+				buttonState = ButtonState.ENCRYPTING;
+				progressBar1.Value = 0;
+				Thread thread = new Thread(new ThreadStart(startEncoding));
+				thread.Start();
+			}
+			else
+			{
+				buttonState = ButtonState.START;
+			}
+		}
+		void thirdStateClickCancel()
+		{
+			buttonState = ButtonState.CANCELING;
+		}
+		void startEncoding()
+		{
+			using (FileStream fsSource = new FileStream(file.FullName, FileMode.Open, FileAccess.ReadWrite)) // открываем файл
+			{
+				int blockSize = (int)Math.Min(numericUpDown1.Value, fsSource.Length);
+				byte[] bytes = new byte[blockSize];
+				progressBar1.Invoke(new Action(() =>
+				{
+					progressBar1.Maximum = (int)fsSource.Length;
+					progressBar1.Step = blockSize;
+				}));
+				DateTime dateTime = DateTime.Now;
+				int speed = 0;
+				long? revers_pos = null;
+				int reader_bytes;
+				string key = textBoxKey.Text;
+				while ((reader_bytes = fsSource.Read(bytes, 0, blockSize)) > 0)
+				{
+					if(revers_pos == null && buttonState == ButtonState.CANCELING)
+					{
+						revers_pos = fsSource.Position;
+						fsSource.Position = 0;
+						progressBar1.Invoke(new Action(() =>
+						{
+							progressBar1.Step = -blockSize;
+						}));
+						continue;
+					}
+
+					if (dateTime != DateTime.Now)
+					{
+						dateTime = DateTime.Now;
+						this.Invoke(new Action(() => toolTip1.SetToolTip(progressBar1, $"{speed/1024} Kb/s")));
+						speed = 0;
+					}
+
+					fsSource.Position -= reader_bytes;
+					Thread.Sleep(50); // REMOVE FOR FAST WORK
+					
+					for (int i = 0; i < blockSize; i++)
+					{
+						if (revers_pos == null && buttonState == ButtonState.CANCELING)
+							break;
+						bytes[i] ^= (byte)key[i % key.Length];
+					//	fsSource.WriteByte(bytes[i]);
+						speed++;
+					}
+
+					if (revers_pos == null && buttonState == ButtonState.CANCELING)
+						continue;
+					fsSource.Write(bytes, 0, reader_bytes);
+					progressBar1.Invoke(new Action(() => {
+						progressBar1.PerformStep();
+					}));
+
+					if (revers_pos == fsSource.Position + reader_bytes)
+					{
+						MessageBox.Show("Canceled");
+						buttonState = ButtonState.START;
+						return;
+					}
+				}
+			}
+			MessageBox.Show("File was crypted succesfully!");
+			buttonState = ButtonState.START;
 		}
 	}
 }
